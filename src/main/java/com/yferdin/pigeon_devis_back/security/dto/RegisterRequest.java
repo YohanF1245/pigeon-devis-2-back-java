f78004499e9055121938cb0875658b6c62b45ea6
Yohan Ferdin <yohan.ferdin@hotmail.fr>
2025-01-19 14:25:54 +0100

    feat(RegisterRequest.java): use StrongPassword validation

--- src/main/java/com/yferdin/pigeon_devis_back/security/dto/RegisterRequest.java
+++ src/main/java/com/yferdin/pigeon_devis_back/security/dto/RegisterRequest.java
@@ -1,5 +1,6 @@
 package com.yferdin.pigeon_devis_back.security.dto;
 
+import com.yferdin.pigeon_devis_back.security.validation.StrongPassword;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
@@ -15,7 +16,7 @@ public class RegisterRequest {
     private String email;
 
     @NotBlank(message = "Le mot de passe est obligatoire")
-    @Size(min = 8, message = "Le mot de passe doit contenir au moins 8 caractères")
+    @StrongPassword
     private String password;
 
     @NotBlank(message = "Le prénom est obligatoire")
