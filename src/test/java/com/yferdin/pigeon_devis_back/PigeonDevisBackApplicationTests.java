d75d9e0b844c23b2daf71f6bc2da6b8383d32829
Yohan Ferdin <yohan.ferdin@hotmail.fr>
2025-01-19 17:02:26 +0100

    test(PigeonDevisBackApplicationTests.java): update application context tests

--- src/test/java/com/yferdin/pigeon_devis_back/PigeonDevisBackApplicationTests.java
+++ src/test/java/com/yferdin/pigeon_devis_back/PigeonDevisBackApplicationTests.java
@@ -2,8 +2,10 @@ package com.yferdin.pigeon_devis_back;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class PigeonDevisBackApplicationTests {
 
 	@Test
