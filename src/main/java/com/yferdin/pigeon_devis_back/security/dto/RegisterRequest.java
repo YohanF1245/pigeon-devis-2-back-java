3493c392882888483f09128943acba9cac68854d
Yohan Ferdin <yohan.ferdin@hotmail.fr>
2025-01-19 17:00:55 +0100

    refactor(RegisterRequest.java): update validation constraints

--- src/main/java/com/yferdin/pigeon_devis_back/security/dto/RegisterRequest.java
+++ src/main/java/com/yferdin/pigeon_devis_back/security/dto/RegisterRequest.java
@@ -3,6 +3,7 @@ package com.yferdin.pigeon_devis_back.security.dto;
 import com.yferdin.pigeon_devis_back.security.validation.StrongPassword;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
@@ -28,5 +29,6 @@ public class RegisterRequest {
     private String lastName;
 
     @Size(max = 15, message = "Le numéro de téléphone ne doit pas dépasser 15 caractères")
+    @Pattern(regexp = "^(?:(?:\\+|00)33|0)\\s*[1-9](?:[\\s.-]*\\d{2}){4}$", message = "Le numéro de téléphone doit être au format français")
     private String phone;
 } 
