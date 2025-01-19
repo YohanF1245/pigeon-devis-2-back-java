2765e0475bbae68e69e87fd1a8608020ecbfe3e5
Yohan Ferdin <yohan.ferdin@hotmail.fr>
2025-01-19 14:40:36 +0100

    fix(AuthResponse): add NoArgsConstructor for JSON deserialization

--- src/main/java/com/yferdin/pigeon_devis_back/security/dto/AuthResponse.java
+++ src/main/java/com/yferdin/pigeon_devis_back/security/dto/AuthResponse.java
@@ -2,10 +2,12 @@ package com.yferdin.pigeon_devis_back.security.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
 @AllArgsConstructor
 public class AuthResponse {
     private String token;
