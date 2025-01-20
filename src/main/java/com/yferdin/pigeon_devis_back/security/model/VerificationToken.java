474ddfac4b168bd57871f43c6318b108bd2f37aa
Yohan Ferdin <yohan.ferdin@hotmail.fr>
2025-01-21 00:19:28 +0100

    refactor(VerificationToken.java): use UUID and add constraints

--- src/main/java/com/yferdin/pigeon_devis_back/security/model/VerificationToken.java
+++ src/main/java/com/yferdin/pigeon_devis_back/security/model/VerificationToken.java
@@ -4,6 +4,7 @@ import com.yferdin.pigeon_devis_back.user.model.User;
 import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
@@ -15,15 +16,19 @@ import java.util.UUID;
 public class VerificationToken {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    @GeneratedValue(generator = "uuid2")
+    @GenericGenerator(name = "uuid2", strategy = "uuid2")
+    @Column(columnDefinition = "UUID")
+    private UUID id;
 
+    @Column(unique = true, nullable = false)
     private String token;
 
     @OneToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id")
+    @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
+    @Column(name = "expiry_date", nullable = false)
     private LocalDateTime expiryDate;
 
     public VerificationToken(User user) {
