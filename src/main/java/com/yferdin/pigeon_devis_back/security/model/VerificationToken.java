a8911b410f51c7596dcbf89625677d69b09a0cb8
Yohan Ferdin <yohan.ferdin@hotmail.fr>
2025-01-20 18:46:13 +0100

    feat(VerificationToken.java): add token entity

---
+++ src/main/java/com/yferdin/pigeon_devis_back/security/model/VerificationToken.java
@@ -0,0 +1,38 @@
+package com.yferdin.pigeon_devis_back.security.model;
+
+import com.yferdin.pigeon_devis_back.user.model.User;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@Entity
+@NoArgsConstructor
+@Table(name = "verification_tokens")
+public class VerificationToken {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String token;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    private LocalDateTime expiryDate;
+
+    public VerificationToken(User user) {
+        this.user = user;
+        this.token = UUID.randomUUID().toString();
+        this.expiryDate = LocalDateTime.now().plusHours(24);
+    }
+
+    public boolean isExpired() {
+        return LocalDateTime.now().isAfter(this.expiryDate);
+    }
+} 
